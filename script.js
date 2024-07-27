f7601671a9e879b63c9e65d2bfcfe43af699ffdd
iamdrawlinsDevdesigner <iamrawlinsDevdesigner>
2024-07-27 21:21:20 +0100

    updated script

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-function doPost(e){
+function doGet(e) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+    var data = sheet.getDataRange().getValues();
+    
+    var jsonArray = [];
+    for (var i = 1; i < data.length; i++) {
+      var rowObject = {};
+      rowObject["image"] = data[i][0];
+      rowObject["title"] = data[i][1];
+      rowObject["description"] = data[i][2];
+      rowObject["price"] = data[i][3];
+      rowObject["location"] = data[i][4];
+      jsonArray.push(rowObject);
+    }
+    
+    return ContentService.createTextOutput(JSON.stringify(jsonArray)).setMimeType(ContentService.MimeType.JSON);
+  }
   
+  function doPost(e) {
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
     var data = JSON.parse(e.postData.contents);
     sheet.appendRow([data.image, data.title, data.description, data.price, data.location]);
     return ContentService.createTextOutput(JSON.stringify({ "result": "success" })).setMimeType(ContentService.MimeType.JSON);
-  
-  }
-
-  function doGet(e) {
-    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-    var data = sheet.getDataRange().getValues();
-    var jsonData = [];
-    
-    for (var i = 1; i < data.length; i++) {
-      var row = data[i];
-      jsonData.push({
-        image: row[0],
-        title: row[1],
-        description: row[2],
-        price: row[3],
-        location: row[4]
-      });
-    }
-    
-    return ContentService.createTextOutput(JSON.stringify(jsonData)).setMimeType(ContentService.MimeType.JSON);
   }
   
